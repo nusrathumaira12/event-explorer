8dd35107fe8851971d0deaeb40fd7a98fa1945a8
nusrathumaira12 <nusrathum31@gmail.com>
2025-05-08 23:21:37 +0600

    updated banner

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -43,15 +43,15 @@ const Banner = () => {
         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
             <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
                 <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-                    {/* International */}
+                   
                 {slide.title.split(' ').slice(0, -1).join(' ')}  <br />
                     <span className="text-violet-600">{slide.title.split(' ').slice(-1)}
-                        {/* Business */}
+                      
                         </span>
                       
                 </h1>
                 <p className="mt-6 mb-8 text-lg sm:mb-12"> {slide.description}
-                    {/* <br  className="hidden md:inline lg:hidden" />Explore, connect, and create—FusionSphere is where the future of creative events begins. */}
+                   
                 </p>
                 <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                     <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Explore</a>
